d9348db69f1fe89af06e9614c33f64ca646613bf
grobin05 <grantgrobinson@gmail.com>
2021-01-29 12:16:41 -0800

    enter

---
+++ pythonin5.py
@@ -0,0 +1,24 @@
+# My Comment
+"""
+faskjd;ljf
+sa;sldkj;
+oispknw
+"""
+1+3
+4-2
+9*5
+10/4
+a = "hello world"
+b = ["Hello World", "Francis"]
+print(a)
+print(b)
+print(b[1])
+print(b + [1,2,3,4,5])
+c = {"favcolor":"hello world"}
+print(c["favcolor"])
+d = {1,2,3,4,5,5,5,5,6,6,6,6}
+print(d)
+if 1 == 1:
+    print("Of course")
+if 1 == 2:
+    print("Obviously not")
